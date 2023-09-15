f7d5b02660ef9bc69c1217fcb98f7f21c6f9dc9d
MaxSurm <surmontmaxime@gmail.com>
2023-09-15 17:06:28 +0200

    front ok

--- src/views/FrontClient.jsx
+++ src/views/FrontClient.jsx
@@ -54,7 +54,7 @@ export const FrontClient = () => {
                 <img id="big-logo" src="/public/img/Logo.png" alt="logo" />
             </header>
             <main className='flex justify-center my-20'>
-                <div className="bg-[#DBFFD6] rounded-2xl sm:w-5/6 sm:mx-5 lg:w-1/2">
+                <div className="bg-[#DBFFD6] rounded-2xl sm:w-5/6 sm:mx-5 lg:w-1/2 shadow-lg shadow-slate-500">
                     <div className="bg-[#50A34E] rounded-t-2xl text-white font-bold px-5 py-2">
                         <h1 className='text-center'>{questions[n].question}</h1>
                     </div>
@@ -98,7 +98,7 @@ export const FrontClient = () => {
                     )}
                     <div className='flex justify-around pb-8'>
                         {questions[n].rankOrder > 1 && <button onClick={prevQuestion} className='bg-[#317845] w-1/3 text-white font-bold rounded-xl p-2'>Précédent</button>}
-                        <button onClick={nextQuestion} className='bg-[#317845] w-1/3 text-white font-bold rounded-xl p-2'>Suivant</button>
+                        {questions[n].rankOrder === questions.length -1 ? <button className='bg-[#317845] w-1/3 text-white font-bold rounded-xl p-2'>Envoyer le formulaire</button> : <button onClick={nextQuestion} className='bg-[#317845] w-1/3 text-white font-bold rounded-xl p-2'>Suivant</button>}
                     </div>
                 </div>
             </main>
